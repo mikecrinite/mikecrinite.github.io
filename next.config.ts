89b8cfb70e404dcb225718a09590376a67e4746b
mikecrinite <mikecrinite@gmail.com>
2025-01-30 16:52:56 -0500

    Just trying a different workflow script entirely

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: "export",
-    reactStrictMode: true,
+    // reactStrictMode: true,
+    basePath: "/",
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
