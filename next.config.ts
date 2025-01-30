05064bcd1b3cb8074b57dd686ddcd44eaca25477
mikecrinite <mikecrinite@gmail.com>
2025-01-30 16:54:17 -0500

    basepath has to be either empty or a prefix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
     output: "export",
     // reactStrictMode: true,
-    basePath: "/",
+    basePath: "",
     images: {
         unoptimized: true,
     }
