5cf0470f3f19cad07977bdb4459ace0f11be0079
mikecrinite <mikecrinite@gmail.com>
2025-02-02 19:17:10 -0500

    Add Header with nav menu, fix layouts

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,26 +1,28 @@
 import Image from "next/image";
 import {spaceMono} from "@/components/fonts";
-import {Container, Space, Stack, Title} from "@mantine/core";
+import {Center, Container, Space, Stack, Title} from "@mantine/core";
 import Links from "@/components/links";
 
 export default function Home() {
     return (
         <Stack justify={'center'} gap={'lg'}>
-            <Space h="lg" />
-            <Image
-                src={"/Mike_Fushimi_Inari_20191009_160250.jpg"}
-                alt={"Me on my 2019 solo trip to Japan"}
-                width={3456}
-                height={4608}
-                style={{
-                    display: 'block',
-                    borderRadius: '10%',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    height : '30%',
-                    width: '30%',
-                }}
-            />
+            {/* kinda wonky... todo: figure out a better way here*/}
+            <Space h="xl"/>
+            <Space h="xl"/>
+            <Center>
+                <Image
+                    src={"/Mike_Fushimi_Inari_20191009_160250.jpg"}
+                    alt={"Me on my 2019 solo trip to Japan"}
+                    width={3456}
+                    height={4608}
+                    style={{
+                        display: 'block',
+                        borderRadius: '10%',
+                        height : '30%',
+                        width: '30%',
+                    }}
+                />
+            </Center>
             <Container>
                 <Title order={1} className={`${spaceMono.className}`}>
                     Michael Crinite
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Home from "@/components/home";
 import {Center} from "@mantine/core";
+import Header from "@/components/header";
 
 export default function HomePage() {
     return (
         <>
+            <Header/>
             <Center>
                 <Home/>
             </Center>
